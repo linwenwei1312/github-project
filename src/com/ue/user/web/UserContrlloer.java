d5d666a830bbf38141dbf4b34be47d1acf1fac6f
linwenwei1312 <466765225@qq.com>
2016-12-11 11:19:46 +0800

    项目提交

--- src/com/ue/user/web/UserContrlloer.java
+++ src/com/ue/user/web/UserContrlloer.java
@@ -4,5 +4,7 @@ package com.ue.user.web;
  * Created by Administrator on 2016/12/11.
  */
 public class UserContrlloer {
-
+    public static void main(String[] args) {
+        System.out.print("hello world");
+    }
 }
